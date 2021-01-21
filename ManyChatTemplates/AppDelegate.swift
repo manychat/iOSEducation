f190d0fe04a739791e2bd8aa14828a54c20cc69f
aleksiosdev <aleksios@manychat.com>
2021-01-21 21:27:13 +0300

    Project template

---
+++ ManyChatTemplates/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  ManyChatTemplates
+//
+//  Created by Aleksandr Lavrinenko on 21.01.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+}
+
