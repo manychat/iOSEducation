e0c1520dfd8d0f7d340583c6db8dc14f2d905583
Alexander Maslyaev <maslyaevalexandr@gmail.com>
2021-01-22 16:12:53 +0300

    Start working

--- ManyChatTemplates/AppDelegate.swift
+++ ManyChatTemplates/AppDelegate.swift
@@ -12,7 +12,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
+        window = UIWindow()
+        
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        
+        window?.makeKeyAndVisible()
 		return true
 	}
 }
--- ManyChatTemplates/ViewController.swift
+++ ManyChatTemplates/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let label: UILabel = UILabel()
+    
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(label)
+        
+        label.text = "ManyChat Template Store"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        let constraint = label.topAnchor.constraint(equalTo: view.topAnchor, constant: 50)
+        constraint.isActive = true
+        
+        [
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ].forEach { constraint in
+            constraint.isActive = true
+        }
+        label.textAlignment = .center
 	}
-
-
 }
-
