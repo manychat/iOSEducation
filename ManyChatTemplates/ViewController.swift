9987112fb93977404e33786dde4422da362328b8
aleksiosdev <aleksios@manychat.com>
2021-01-22 14:38:26 +0300

    First lesson

--- ManyChatTemplates/ViewController.swift
+++ ManyChatTemplates/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+	let label = UILabel()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+
+		view.backgroundColor = .white
+		view.addSubview(label)
+
+		label.text = "Test"
+		label.translatesAutoresizingMaskIntoConstraints = false
+
+		[
+			label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+			label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
+			label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+		].forEach { constraint in
+			constraint.isActive = true
+		}
+		label.textAlignment = .center
 	}
-
-
 }
-
